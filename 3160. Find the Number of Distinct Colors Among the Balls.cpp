b6845faf1df3291c201d59be71c86a90c6accaed
Mangesh Jaybhaye <132568893+MangeshJ10603@users.noreply.github.com>
2025-03-10 15:36:07 +0530

    Create 3160. Find the Number of Distinct Colors Among the Balls.cpp

---
+++ 3160. Find the Number of Distinct Colors Among the Balls.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        int n = queries.size();
+        vector<int> res(n);
+        unordered_map<int, int> colormp;
+        unordered_map<int, int> ballmp;
+        for (int i = 0; i < n; i++) {
+            int ball = queries[i][0];
+            int color = queries[i][1];
+            if (ballmp.count(ball)) {
+                int prevcolor = ballmp[ball];
+                colormp[prevcolor]--;
+                if (colormp[prevcolor] == 0) {
+                    colormp.erase(prevcolor);
+                }
+            }
+            ballmp[ball] = color;
+            colormp[color]++;
+            res[i] = colormp.size();
+        }
+        return res;
+    }
+};
