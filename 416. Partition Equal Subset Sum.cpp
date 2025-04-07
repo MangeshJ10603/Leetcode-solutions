ff263eb5b326d24856513db089b05488c6661de9
Mangesh Jaybhaye <132568893+MangeshJ10603@users.noreply.github.com>
2025-04-07 13:59:07 +0530

    Create 416. Partition Equal Subset Sum.cpp

---
+++ 416. Partition Equal Subset Sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int t[201][20001];
+    bool solve(vector<int>& nums, int i, int x){
+        if(x==0) return true;
+        if(i>=nums.size()) return false;
+        if(t[i][x]!=-1) return t[i][x];
+        bool take=false;
+        if(nums[i]<=x){
+            take=solve(nums,i+1,x-nums[i]);
+        }
+
+        bool not_take=solve(nums,i+1,x);
+        return t[i][x]=take || not_take;
+    }
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int S=accumulate(nums.begin(),nums.end(),0);
+        if(S%2!=0) return false;
+        int x=S/2;
+        memset(t,-1,sizeof(t));
+        return solve(nums,0,x);
+    }
+};
